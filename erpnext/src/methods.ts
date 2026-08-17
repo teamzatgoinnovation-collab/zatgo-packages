c8eba1738c4625f7166bec46caf37681eb75ea5e
ZatGo Innovation <team.zatgoinnovation@gmail.com>
2026-08-17 17:38:30 +0530

    Remove dead paymentsCreateContra mapping (superseded by journalsCreate)

--- erpnext/src/methods.ts
+++ erpnext/src/methods.ts
@@ -166,7 +166,6 @@ export const ZatGoApi = {
     paymentsGet: ZatGoCoreApi.accounting.payments.get,
     paymentsCreateReceive: ZatGoCoreApi.accounting.payments.createReceive,
     paymentsCreatePay: ZatGoCoreApi.accounting.payments.createPay,
-    paymentsCreateContra: "zatgo_core.api.v1.accounting.payments.create_contra",
     paymentsCreateReceiveAdvance: "zatgo_core.api.v1.accounting.payments.create_receive_advance",
     paymentsCreatePayAdvance: "zatgo_core.api.v1.accounting.payments.create_pay_advance",
     paymentsSubmit: ZatGoCoreApi.accounting.payments.submit,
